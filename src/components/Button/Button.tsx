6ca809d8200212ecf6ac55b0aa9a6f4f35396c2d
Rectmoon <15692087320@163.com>
2021-03-13 16:46:18 +0800

    fix: scss

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,10 @@ import { IButtonProps } from "./Button.types";
 
 import "./Button.scss";
 
+const prefix = 'rainbow'
+
 const Button: React.FC<IButtonProps> = ({ foo }) => (
-  <div data-testid="Button" className="my-button">
+  <div data-testid="Button" className={`${prefix}-button`}>
     {foo}
     Button
   </div>
